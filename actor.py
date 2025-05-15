768bc0848d502052f632452b5624737a8b6265b0
Sarthak Dayal <dayalsarthak5@gmail.com>
2025-05-15 00:13:23 -0500

    Fix misspelling

--- actor.py
+++ actor.py
@@ -10,20 +10,20 @@ class DQNAgent:
         self.device = device
         self.gamma = gamma
         self.criterion = nn.MSELoss()
-        self.optim = torch.optim.Adam(self.net.parameters(), lr=1e-4)
+        self.optim = torch.optim.Adam(self.policy_net.parameters(), lr=1e-4)
     
     def act(self, obs, epsilon=0.0):
         if torch.rand(1) < epsilon:
-            return torch.randint(self.net.out_shape, (1,)).to(self.device)
+            return torch.randint(self.policy_net.out_shape, (1,)).to(self.device)
         
-        return torch.argmax(self.net(obs), dim=1).to(self.device)
+        return torch.argmax(self.policy_net(obs), dim=1).to(self.device)
 
     def train_step(self, batch):
-        Q_pred = self.net(torch.stack(batch.state).to(self.device))
+        Q_pred = self.policy_net(torch.stack(batch.state).to(self.device))
         Q_pred = torch.gather(Q_pred, 1, torch.stack(batch.action).to(self.device))
         
         with torch.no_grad():
-            nextQ = self.net(torch.stack(batch.next_state).to(self.device))
+            nextQ = self.policy_net(torch.stack(batch.next_state).to(self.device))
         
             # if a state is done, do not bootstrap
             # dim 0 is batch dim, dim 1 should be a bunch of Q values for all actions, we want the max
